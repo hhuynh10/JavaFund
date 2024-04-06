bbdd9224f34a6d997815445838b66c43dda7691c
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-04-06 14:25:24 -0500

    study

--- Chapter7/7.5/Problem.java
+++ Chapter7/7.5/Problem.java
@@ -1,18 +1,36 @@
-//import API's needed here:
-import java.util.Arrays;
 import java.util.Scanner;
-public class Problem 
-{  
-        public static void main(String args[ ]) 
-        {
-                int[] numArr = new int[10];
-                Scanner input = new Scanner(System.in);
-                System.out.print("Enter 10 numbers: ");
 
-                for (int i = 0; i < numArr.length; i++){
-                        numArr[i] = input.nextInt();
+public class Problem {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        int[] numArr = new int[10];
+        int count = 0;
+
+        System.out.print("Enter 10 numbers: ");
+        for (int i = 0; i < 10; i++) {
+            int userInput = input.nextInt();
+            boolean isNew = true;
+
+            for (int j = 0; j < count; j++) {
+                if (numArr[j] == userInput) {
+                    isNew = false;
+                    break;
                 }
+            }
 
-                System.out.println(Arrays.toString(numArr));
-         } //closing main method
-} //closing class header
+            if (isNew) {
+                numArr[count] = userInput;
+                count++;
+            }
+        }
+
+        System.out.println("The number of distinct numbers is: " + count);
+
+        System.out.print("The distinct numbers are: ");
+        for (int i = 0; i < count; i++) {
+            System.out.print(numArr[i] + " ");
+        }
+
+    } // closing main method
+} // closing class header
