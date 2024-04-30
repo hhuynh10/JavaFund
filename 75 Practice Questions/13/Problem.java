f2e59e31beeaff78da9654baffac4e62f2834244
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-04-30 11:11:07 -0500

    study

--- 75 Practice Questions/13/Problem.java
+++ 75 Practice Questions/13/Problem.java
@@ -3,20 +3,21 @@ public class Problem {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int userInput;
-        int prevInput = 0;
         int count = 1;
         int countDistinct = 0;
+        int[] arr = new int[10000];
 
         while (count < 2){
             System.out.print("Enter an int: ");
             userInput = input.nextInt();
-            if (prevInput == userInput){
-                count++;
-            } else {
-                prevInput = userInput;
+            for (int i = 0; i <= countDistinct; i++){
+                if (arr[i] == userInput){
+                    count++;
+                }
             }
+            arr[countDistinct] = userInput;
             countDistinct++;
         }
-        System.out.println("You entered " + (countDistinct - 1) + (countDistinct - 1 > 1 ? " values." : " value."));
+        System.out.println("You entered " + (countDistinct) + (countDistinct > 1 ? " values." : " value."));
     }
 }
