14911ec4e453dedc4033869dab4f58ee6202fcc0
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-05-01 20:04:27 -0500

    study

--- 75 Practice Questions/53/Problem.java
+++ 75 Practice Questions/53/Problem.java
@@ -1,9 +1,23 @@
 public class Problem {
     public static void main(String[] args){
-        
+        System.out.println(highestPow(30));
+        System.out.println(highestPow(107));
+        System.out.println(highestPow(36));
+        System.out.println(highestPow(80));
+        System.out.println(highestPow(40));
     }
 
     public static int highestPow(int n) {
+        if (n % 2 != 0){
+            return 1;
+        }
 
+        int result = 0;
+        for (int i = 2; i < n; i *= 2){
+            if (n % i == 0){
+                result = i;
+            }
+        }
+        return result;
     }
 }
