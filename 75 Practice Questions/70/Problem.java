78b6fed1515f626936bca2708368767eff9f3a84
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-05-03 08:16:45 -0500

    study

--- 75 Practice Questions/70/Problem.java
+++ 75 Practice Questions/70/Problem.java
@@ -8,10 +8,13 @@ public class Problem {
     public static int[] fibNumbers(int n) {
         int[] arr = new int[n + 1];
         if (n == 0){
-            arr = {0};
+            arr[0] = 0;
+            return arr;
         }
         if (n == 1){
-            arr = {0, 1};
+            arr[0] = 0;
+            arr[1] = 1;
+            return arr;
         }
         arr[1] = 1;
         for (int i = 2; i < arr.length; i++){
---
+++ 75 Practice Questions/71/Problem.java
@@ -0,0 +1,9 @@
+public class Problem {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int[][] switchColumns(int[][] a, int x, int y) {
+        
+    }
+}
