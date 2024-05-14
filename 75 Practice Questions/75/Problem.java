e92de96fd0894a8c268eaaef01454e401919148a
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-05-14 13:59:59 -0500

    study

--- 75 Practice Questions/75/Problem.java
+++ 75 Practice Questions/75/Problem.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
 public class Problem {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int userInput = input.nextInt();
+        char[][] arr = plusInSquare(userInput);
+
+        for (int i = 0; i < arr.length; i++){
+            for ( int j = 0; j < arr[i].length; j++){
+                System.out.printf("%-4c", arr[i][j]);
+            }
+            System.out.println();
+        }
     }
 
     public static char[][] plusInSquare(int n) {
-        
+        char[][] arr = new char[n][n];
+        for (int i = 0; i < arr.length; i++){
+            for ( int j = 0; j < arr[i].length; j++){
+                if (i != 0 && i != n/2 && i != n-1){
+                    if (j != 0 && j != n/2 && j != n-1){
+                        arr[i][j] = '.';
+                    } else {
+                        arr[i][j] = 'X';
+                    }
+                } else {
+                    arr[i][j] = 'X';
+                }
+            }
+        }
+        return arr;
     }
 }
