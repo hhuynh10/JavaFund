1968f75e8c2a07f2e14b16c5393068d4825f36ff
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-05-15 10:14:31 -0500

    study

--- Test/Problem.java
+++ Test/Problem.java
@@ -1,19 +1,13 @@
 public class Problem {
     public static void main(String[] args) {
-        String p = "DHGqwT6";
-        System.out.println(password(p));
+        System.out.println(reverse(123));
+        System.out.println(reverse(-123));
+        System.out.println(reverse(120));
     }
 
-    public static boolean password(String s){
-        if (!(s.length() >= 6 && s.length() < 10)){
-            return false;
-        }
+    public int reverse(int x) {
+        x = Integer.toString(x);
         
-        for (int i = 0; i < s.length(); i++){
-            if (!((s.charAt(i) >= 48 && s.charAt(i) <= 57) || (s.charAt(i) >= 65 && s.charAt(i) <= 90))){
-                return false;
-            }
-        }
-        return true;
+        return Integer.parseInt(x);
     }
 }
