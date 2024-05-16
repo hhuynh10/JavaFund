c3a24bc08a683dcd7a8bf3ac788cac980e760412
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-05-16 08:14:28 -0500

    study

--- Test/Problem.java
+++ Test/Problem.java
@@ -1,13 +1,30 @@
+import java.util.Arrays;
+
 public class Problem {
     public static void main(String[] args) {
-        System.out.println(reverse(123));
-        System.out.println(reverse(-123));
-        System.out.println(reverse(120));
+        String s1 = "anagram";
+        String s2 = "nagaram";
+        String s3 = "rat";
+        String s4 = "car";
+
+        System.out.println(isAnagram(s1, s2));
+        System.out.println(isAnagram(s3, s4));
     }
 
-    public int reverse(int x) {
-        x = Integer.toString(x);
-        
-        return Integer.parseInt(x);
+    public static boolean isAnagram(String s, String t) {
+        int[] sCount = new int[26];
+        int[] tCount = new int[26];
+
+        for (char c : s.toCharArray()){
+            sCount[c - 'a']++;
+        }
+        for (char c : t.toCharArray()){
+            tCount[c - 'a']++;
+        }
+
+        if (Arrays.equals(sCount, tCount)){
+            return true;
+        }
+        return false;
     }
 }
