e2329a64913be523db230d02fb9987ed5bcd89c8
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-06-06 09:08:25 -0500

    study

--- Data Structure Java/Chapter 2/2.12/Problem.java
+++ Data Structure Java/Chapter 2/2.12/Problem.java
@@ -1,5 +1,19 @@
-package Data Structure Java.Chapter 2.2.12;
+import java.util.Arrays;
 
 public class Problem {
-    
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        shuffle(a);
+    }
+
+    public static void shuffle(int[] a){
+        for (int i = 0; i < a.length-1; i++){
+            int j = (int) (Math.random() * a.length);
+
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        System.out.println(Arrays.toString(a));
+    }
 }
