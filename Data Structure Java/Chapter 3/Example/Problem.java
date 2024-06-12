2664333df53d335246ec683641f3f414a8a94191
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-06-11 22:55:03 -0500

    study

--- Data Structure Java/Chapter 3/Example/Problem.java
+++ Data Structure Java/Chapter 3/Example/Problem.java
@@ -5,7 +5,16 @@ public class Problem {
         for (int i = 1; i < 4; i++){
             p = p.next = new Node(22 + 11*i);
         }
-
+        for (p = start; p != null; p = p.next){
+            System.out.println(p.data);
+        }
+        System.out.println();
+        start = insert(start, 45);
+        for (p = start; p != null; p = p.next){
+            System.out.println(p.data);
+        }
+        System.out.println();
+        start = delete(start, 55);
         for (p = start; p != null; p = p.next){
             System.out.println(p.data);
         }
@@ -22,6 +31,24 @@ public class Problem {
             p = p.next;
         }
         p.next = new Node(x, p.next);
+        return start;
+    }
+
+    public static Node delete(Node start, int x){
+        if (start == null || start.data > x){
+            return start;
+        } else if (start.data == x){
+            return start.next;
+        }
+
+        for (Node p = start; p.next != null; p = p.next){
+            if (p.next.data > x){
+                break;
+            } else if (p.next.data == x){
+                p.next = p.next.next;
+                break;
+            }
+        }
         return start;
     }
 }
