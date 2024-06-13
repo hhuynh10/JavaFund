a4c1e4689b6e42e22aca5d5600c6d66a80039d67
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-06-12 21:56:35 -0500

    study

--- Data Structure Java/Chapter 3/3.2/Problem.java
+++ Data Structure Java/Chapter 3/3.2/Problem.java
@@ -2,19 +2,19 @@ public class Problem {
     public static void main(String[] args) {
         Node start = new Node(33);
         Node p = start;
-        for (int i = 1; i < 5; i++){
-            p = p.next = new Node(33 + 11*i);
+        for (int i = 1; i < 5; i++) {
+            p = p.next = new Node(33 + 11 * i);
         }
-        for (p = start; p != null; p = p.next){
+        for (p = start; p != null; p = p.next) {
             System.out.println(p.data);
         }
 
-        System.out.println(size(p));
+        System.out.println(size(start)); // Pass 'start' instead of 'p' to size method
     }
 
-    public static int size(Node list){
+    public static int size(Node list) {
         int count = 0;
-        while (list != null){
+        while (list != null) {
             count++;
             list = list.next;
         }
@@ -25,7 +25,7 @@ public class Problem {
 class Node {
     int data;
     Node next;
-    Node (int data){
+    Node(int data) {
         this.data = data;
     }
 }
