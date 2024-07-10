b34b357f0b9695d8097720b2be83ffc09e3ee9b4
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-07-09 22:34:58 -0500

    study

---
+++ Grokking_Algo/Recursion/Problem.java
@@ -0,0 +1,16 @@
+public class Problem {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+        System.out.println(power(2, 5));
+    }
+
+    public static int factorial(int num){
+        if (num <= 1) return 1;
+        return num * factorial(num-1);
+    }
+
+    public static int power(int base, int exponent){
+        if (exponent < 1) return 1;
+        return base * power(base, exponent-1);
+    }
+}
