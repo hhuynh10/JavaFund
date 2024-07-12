d471e7ed11a5b3e8f2749c7ebe13891983f9daf0
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-07-12 17:30:26 -0500

    study

--- Grokking_Algo/Recursion/Problem.java
+++ Grokking_Algo/Recursion/Problem.java
@@ -11,6 +11,7 @@ public class Problem {
 
     public static int power(int base, int exponent){
         if (exponent < 1) return 1;
+        System.out.println();
         return base * power(base, exponent-1);
     }
 }
