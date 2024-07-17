65bd42df7c68a6935bf81d16a364c2134be9ae97
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-07-16 22:52:25 -0500

    study

--- Grokking_Algo/Stack/problem.java
+++ Grokking_Algo/Stack/problem.java
@@ -3,7 +3,22 @@ import java.util.Stack;
 public class problem {
     public static void main(String[] args) {
         Stack<String> stack = new Stack<>();
+        
         System.out.println(stack.empty());
+
+        stack.push("a1b2c3d4");
+        stack.push("xYz9876");
+        stack.push("QWERTYuiop");
+        stack.push("StackOverflow123");
+        stack.push("openAIrocks");
+        System.out.println(stack);
+
+        String last = stack.pop();
+        System.out.println(last);
+
+        System.out.println(stack.peek());
+
+        System.out.println(stack.search("xYz9876"));
     }
 
 
