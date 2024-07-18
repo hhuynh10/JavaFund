f68ae39051b758aed24a5b73d394d22592fae06a
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-07-17 23:01:30 -0500

    study

--- Grokking_Algo/Stack/problem.java
+++ Grokking_Algo/Stack/problem.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class problem {
     public static void main(String[] args) {
-        Stack<String> stack = new Stack<>();
+        Stack<String> stack = new Stack<String>();
         
         System.out.println(stack.empty());
 
