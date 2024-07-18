ef0b63b00b5f02d8058564b19156b0f441802d56
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-07-17 23:12:12 -0500

    study

--- Grokking_Algo/Queue/Problem.java
+++ Grokking_Algo/Queue/Problem.java
@@ -1,5 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Problem {
     public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<String>();
+
+        System.out.println(queue.isEmpty());
+
+        queue.offer("Karen");
+        queue.offer("Steve");
+        queue.offer("Chad");
+        queue.offer("Harold");
+        System.out.println(queue);
         
+        String first = queue.poll();
+        System.out.println(first);
+        
+        System.out.println(queue.peek());
+
+        System.out.println(queue.size());
+
+        System.out.println(queue.contains("Steve"));
     }
 }
--- Grokking_Algo/Stack/problem.java
+++ Grokking_Algo/Stack/problem.java
@@ -18,8 +18,8 @@ public class problem {
 
         System.out.println(stack.peek());
 
+        System.out.println(stack.size());
+
         System.out.println(stack.search("xYz9876"));
     }
-
-
 }
