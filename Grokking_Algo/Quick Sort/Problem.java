eefe01288daa54d293547aff6dbffcb0ff7a2eee
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-08-07 19:46:03 -0500

    study

---
+++ Grokking_Algo/Quick Sort/Problem.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Problem {
+    public static void main(String[] args) {
+        int[] arr = {17,3,9,5,12,20,8,15,4,19,11,1,7,16,10,14,6,18,2,13};
+        System.out.println(Arrays.toString(quickSort(arr)));
+    }
+
+    
+}
